4cf21a084e9960877dfaf7e09dda84cd4f0c65b3
Nate <natethepcspecialist@gmail.com>
2019-07-12 21:39:43 -0400

    update

--- js/main.mjs
+++ js/main.mjs
@@ -6,12 +6,6 @@ const socket = io(),
   canvas = document.getElementById("game"),
   ctx = canvas.getContext("2d");
 
-const writeToCanvas = msg => {
-  ctx.fillStyle = "white";
-  ctx.font = "20px";
-  ctx.fillText(msg, 30, 30);
-};
-
 let players = [],
   items = [],
   endGame;
